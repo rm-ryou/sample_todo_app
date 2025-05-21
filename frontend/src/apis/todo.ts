83273648475934d9a6865670fc034123e28694d6
rm-ryou <rmco.420118@gmail.com>
2025-05-22 00:27:10 +0900

    feat: add api

--- frontend/src/apis/todo.ts
+++ frontend/src/apis/todo.ts
@@ -1,6 +1,16 @@
-export const loader = async () => {
+import type { LoaderFunctionArgs } from 'react-router'
+
+export const todosLoader = async () => {
   const endpoint = import.meta.env.VITE_API_ENDPOINT
   const response = await fetch(`${endpoint}/v1/todos/`)
+  const resData = await response.json()
+
+  return resData
+}
+
+export const todoDetailsLoader = async ({ params }: LoaderFunctionArgs) => {
+  const endpoint = import.meta.env.VITE_API_ENDPOINT
+  const response = await fetch(`${endpoint}/v1/todos/${params.id}`)
   const resData = await response.json()
 
   return resData
