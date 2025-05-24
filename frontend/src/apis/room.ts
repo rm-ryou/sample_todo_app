a62560414e2794045e93e17c58e271024086b482
rm-ryou <rmco.420118@gmail.com>
2025-05-25 00:59:00 +0900

    feat: add rooms api

---
+++ frontend/src/apis/room.ts
@@ -0,0 +1,16 @@
+import type { Rooms } from '@/types'
+
+export const roomsLoader = async () => {
+  const endpoint = import.meta.env.VITE_API_ENDPOINT
+
+  try {
+    const response = await fetch(`${endpoint}/v1/rooms/`)
+    if (!response.ok) {
+      // TODO: handle error
+    }
+    const resData: Rooms = await response.json()
+    return resData
+  } catch (error) {
+    // TODO: handle error
+  }
+}
