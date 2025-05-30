0a480c88d9b70b598fa6368410a72496ea5bb1c0
rm-ryou <rmco.420118@gmail.com>
2025-05-30 22:32:17 +0900

    feat: add delete method for room

--- frontend/src/apis/room.ts
+++ frontend/src/apis/room.ts
@@ -1,3 +1,4 @@
+import type { UniqueIdentifier } from '@dnd-kit/core'
 import type { Rooms } from '@/types'
 
 export const roomsLoader = async () => {
@@ -13,5 +14,29 @@ export const roomsLoader = async () => {
   } catch (error) {
     console.error(error)
     // TODO: handle error
+  }
+}
+
+export const deleteRoom = async (id: UniqueIdentifier) => {
+  const endpoint = import.meta.env.VITE_API_ENDPOINT
+
+  try {
+    const response = await fetch(`${endpoint}/v1/rooms/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+    if (!response.ok) {
+      // FIXME: Make the caller catch the error and display a message
+      const errorData = await response.json().catch(() => null)
+      const errorMessage =
+        errorData?.message || response.statusText || 'Unexpected error'
+      console.error(
+        `Failed to delete room. ${response.status}: ${errorMessage}`,
+      )
+    }
+  } catch (error) {
+    console.error(error)
   }
 }
