f7900ff6be2d444862c5aac7ce7352ce35bcca00
rm-ryou <rmco.420118@gmail.com>
2025-05-30 22:27:11 +0900

    feat: add mock delete handler

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -7,6 +7,12 @@ const endpoint = import.meta.env.VITE_API_ENDPOINT
 
 export const handlers = [
   http.get(`${endpoint}/v1/rooms`, () => {
-    return HttpResponse.json<Rooms>({ rooms: rooms })
+    return HttpResponse.json<Rooms>({ rooms: rooms }, { status: 200 })
+  }),
+
+  http.delete(`${endpoint}/v1/rooms/:id`, ({ params }) => {
+    const { id } = params
+    console.log('Deleting room with ID "%s"', id)
+    return HttpResponse.json({ message: 'ok' }, { status: 200 })
   }),
 ]
